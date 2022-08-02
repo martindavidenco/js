d09b67671268edcddead24a359b497d14ffc92a7
martindavidenco <tinchodavidenco@gmail.com>
2022-08-02 16:45:25 -0300

    desafio

--- js/script.js
+++ js/script.js
@@ -1,20 +1,40 @@
 
-let nacimiento = parseInt(prompt("Ingrese su año de nacimiento"))
-let año = parseInt(prompt("ingrese un año en el futuro"))
-let resultado = año - nacimiento
-let faltan = 65 - resultado
-
-for (resultado; resultado < 70; resultado++) {
-    if (resultado < 18) {
-        console.log("En el año " + año + " sos menor, te faltan " + (65 - resultado) + " años")
-    } else if (resultado < 30) {
-        console.log("En el año " + año + " te faltan " + (65 - resultado) + " años, es bastante tenes que laburar mas papi")
-    } else if (resultado <= 65) {
-        console.log("En el año " + año + " te faltan " + (65 - resultado) + " años, ya casi campeon")
-    } else if (resultado > 65) {
-        console.log("En el año " + año + " ya te jubilaste hace " + (resultado - 65) + " años, a descansar!")
-    } else {
-        console.warn("ingresa un numero");
-    }
-    año++;
+function calculo(producto1, producto2, producto3) {
+    alert("el total de su compra es de $" + (producto1 + producto2 + producto3))
 }
+let entrada = confirm("DESEA COMPRAR UN PRODUCTO?")
+while (entrada) {
+    let producto = prompt("Escribe el/los numero/s del producto que desea comprar. 1 celular 2 funda 3 cargador (Todo junto. Si desea por ejemplo un celular y cargador escriba 13. Si desea las tres cosas 123.");
+    switch (producto) {
+        case "1":
+            alert("usted eligio un celular")
+            calculo(10000, 0, 0)
+            break;
+        case "12":
+            alert("usted eligio un celular y una funda")
+            calculo(10000, 1000, 0)
+            break;
+        case "123":
+            alert("usted eligio un celular, funda y cargador")
+            calculo(10000, 1000, 1200)
+            break;
+        case "23":
+            alert("usted eligio una funda y un cargador")
+            calculo(0, 1200, 1000)
+            break;
+        case "13":
+            alert("usted eligio un celular y un cargador")
+            calculo(10000, 0, 1200)
+            break;
+        case "2":
+            alert("usted eligio una funda")
+            calculo(0, 1000, 0)
+            break;
+        case "3":
+            alert("usted eligio un cargador")
+            calculo(0, 0, 1200)
+            break;
+    }
+    entrada = confirm("Desea seguir con la compra?");
+}
+alert("Gracias por su tiempo")
