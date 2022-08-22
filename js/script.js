6b34066344dd1d2bd49c2a65eda0ecc7812ee51b
martindavidenco <tinchodavidenco@gmail.com>
2022-08-22 19:00:41 -0300

    duda

--- js/script.js
+++ js/script.js
@@ -1,56 +1,104 @@
 const productos = []
 const IVA = 1.21
+let carrito = []
 class Producto {
-    constructor(nombre, importe, talle) {
+    constructor(nombre, importe, talle, id) {
         this.nombre = nombre
         this.importe = importe
         this.talle = talle
+        this.id = id
     }
     precioFinal() {
         return "$ " + parseFloat((this.importe * IVA).toFixed(2))
     }
 }
-productos.push(new Producto("Remera Nike", 3500, "XL"))
-productos.push(new Producto("Zapatillas New Balance", 5000, "44"))
-productos.push(new Producto("Pantalon deportivo", 2000, "XXL"))
-productos.push(new Producto("Gorro", 2200, "L"))
-productos.push(new Producto("Camisa lisa", 3123, "M"))
-productos.push(new Producto("Zapatillas Converse", 5600, "38"))
-productos.push(new Producto("Campera rompevientos vintage", 4200, "S"))
-productos.push(new Producto("Buzo adidas", 4900, "XL"))
+productos.push(new Producto("Buzo Trasher", 4800, "XL", 1))
+productos.push(new Producto("Pantalon de jean clasico", 5000, "XXl", 2))
+productos.push(new Producto("Zapatilla Negra New Air 3.0", 15800, "43", 3))
+productos.push(new Producto("Remera Mc Lovin", 2700, "L", 4))
+productos.push(new Producto("Camiseta deportiva negra", 3123, "M", 5))
+productos.push(new Producto("Gorro Nike", 5600, "38", 6))
+productos.push(new Producto("Medias fogosas", 2300, "40", 7))
+productos.push(new Producto("Pantalon Nike Babucha", 4900, "XL", 8))
 console.table(productos);
 
 function listaHtml() {
     const lista = document.getElementById("lista")
-    lista.innerHTML=""
+    lista.innerHTML = ""
     productos.forEach(producto => {
         lista.innerHTML += `   <div class="card" style="width: 16rem;">
-        <img src="..." class="card-img-top" alt="${producto.nombre} ${producto.talle}">
+        <img src="assets/productos/prod${producto.id}.jpg" class="card-img-top" alt="${producto.nombre} ${producto.talle}">
         <div class="card-body ">
             <h5 class="card-title">${producto.nombre}</h5>
-            <p class="card-text">${producto.importe}</p>
-            <a href="producto.html"  class="btn btn-primary">Comprar</a>
+            <p class="card-text">$ ${producto.importe}</p>
+            <a class="btn btn-primary" id="btn-agregar${producto.id}">Agregar al carrito</a>
         </div>
     </div>`
+    });
+    funcionCarrito();
+}
+function funcionCarrito(){
+    productos.forEach((producto) => {
+        document.querySelector(`#btn-agregar${producto.id}`).addEventListener("click", () => {
+            agregarCarrito(producto)
+        })
     })
 }
+function agregarCarrito(producto){
+    let cantidad = carrito.some(prod => prod.id === producto.id )
+    if (cantidad === false){
+        producto.cantidad = 1
+        carrito.push(producto)
+    }else {
+        let find = carrito.find(productofind => productofind.id === producto.id);
+        find.cantidad++
+    }
+carrito.push(producto);
+console.table(carrito)
+visualizarCarrito()
+}
+function visualizarCarrito(){
+    carritoDiv.innerHTML = "";
+    carrito.forEach((producto)=>{
+        carritoDiv.innerHTML += ` <h2>Tu carrito de compras:</h2>
+        <div> Total:</div>
+        <ul>
+            <li>Producto: ${producto.nombre} </li>
+            <li>Talle: ${producto.talle}</li>
+            <li>Precio: $${producto.importe} </li>
+        </ul>`;
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function agregarProducto() {
     let descripcion = prompt(" ingrese nombre de la prenda de ropa")
     let importe = parseInt(prompt("Ingresa el importe:"))
     let talle = prompt("ingresa el talle")
     productos.push(new Producto(descripcion, importe, talle))
-    
-    
 }
-function cargarProducto(){
-let entrada = confirm("desea cargar un producto?")
-while (entrada) {
-    agregarProducto();
-    console.table(productos)
-    listaHtml()
-    entrada = confirm("desea agregar otro?")
-}
+function cargarProducto() {
+    let entrada = confirm("desea cargar un producto?")
+    while (entrada) {
+        agregarProducto();
+        console.table(productos)
+        listaHtml()
+        entrada = confirm("desea agregar otro?")
+    }
 }
 
 listaHtml()
@@ -74,14 +122,14 @@ function filtrarProductosPrecio() {
     const resultado = productos.filter(elemento => elemento.importe < prod)
     console.table(resultado)
     const lista = document.getElementById("lista")
-    lista.innerHTML=""
+    lista.innerHTML = ""
     resultado.forEach(producto => {
         lista.innerHTML += `   <div class="card" style="width: 16rem;">
         <img src="..." class="card-img-top" alt="${producto.nombre} ${producto.talle}">
         <div class="card-body ">
             <h5 class="card-title">${producto.nombre}</h5>
             <p class="card-text">${producto.importe}</p>
-            <a href="producto.html"  class="btn btn-primary">Comprar</a>
+            <a class="btn btn-primary">Comprar</a>
         </div>
     </div>`
     })
@@ -104,10 +152,10 @@ function ordenarProductosMin() {
     }
     listaHtml()
 }
-
-const boton = document.querySelector("#btn")
-boton.addEventListener ("click", cargarProducto)
+const carritoDiv = document.querySelector("#container")
+const boton = document.querySelector("#botn")
+boton.addEventListener("click", cargarProducto)
 const precio_menor = document.querySelector("#menorPrecio")
-precio_menor.addEventListener ("click",ordenarProductosMin)
+precio_menor.addEventListener("click", ordenarProductosMin)
 const precio_mayor = document.querySelector("#mayorPrecio")
-precio_mayor.addEventListener ("click",filtrarProductosPrecio)
+precio_mayor.addEventListener("click", filtrarProductosPrecio)
