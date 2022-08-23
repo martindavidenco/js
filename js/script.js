6411022fadc1ddb20558870b4d28afa19a354f18
martindavidenco <tinchodavidenco@gmail.com>
2022-08-23 00:37:12 -0300

    carrito

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
+//VARIABLES Y CONSTANTES
 const productos = []
+let carrito = JSON.parse(localStorage.getItem("carrito")) || []
 const IVA = 1.21
-let carrito = []
+//Nodos
+const carritoDiv = document.querySelector("#container")
+const boton = document.querySelector("#botn")
+boton.addEventListener("click", cargarProducto)
+const precio_menor = document.querySelector("#menorPrecio")
+precio_menor.addEventListener("click", ordenarProductosMin)
+const precio_mayor = document.querySelector("#mayorPrecio")
+precio_mayor.addEventListener("click", filtrarProductosPrecio)
+// ARRAY DE OBJETOS
 class Producto {
     constructor(nombre, importe, talle, id) {
         this.nombre = nombre
@@ -21,7 +31,7 @@ productos.push(new Producto("Gorro Nike", 5600, "38", 6))
 productos.push(new Producto("Medias fogosas", 2300, "40", 7))
 productos.push(new Producto("Pantalon Nike Babucha", 4900, "XL", 8))
 console.table(productos);
-
+// DECLARACION FUNCIONES
 function listaHtml() {
     const lista = document.getElementById("lista")
     lista.innerHTML = ""
@@ -37,53 +47,49 @@ function listaHtml() {
     });
     funcionCarrito();
 }
-function funcionCarrito(){
+function funcionCarrito() {
     productos.forEach((producto) => {
         document.querySelector(`#btn-agregar${producto.id}`).addEventListener("click", () => {
             agregarCarrito(producto)
         })
     })
 }
-function agregarCarrito(producto){
-    let cantidad = carrito.some(prod => prod.id === producto.id )
-    if (cantidad === false){
+function agregarCarrito(producto) {
+    let cantidad = carrito.some(prod => prod.id === producto.id)
+    if (cantidad === false) {
         producto.cantidad = 1
         carrito.push(producto)
-    }else {
+    } else {
         let find = carrito.find(productofind => productofind.id === producto.id);
         find.cantidad++
     }
-carrito.push(producto);
-console.table(carrito)
-visualizarCarrito()
+    console.table(carrito)
+    visualizarCarrito()
 }
-function visualizarCarrito(){
+function visualizarCarrito() {
     carritoDiv.innerHTML = "";
-    carrito.forEach((producto)=>{
-        carritoDiv.innerHTML += ` <h2>Tu carrito de compras:</h2>
-        <div> Total:</div>
+    carrito.forEach((producto) => {
+        carritoDiv.innerHTML += ` 
         <ul>
-            <li>Producto: ${producto.nombre} </li>
+            <li>Producto: ${producto.nombre}  <img src="assets/productos/prod${producto.id}.jpg" class="card-img-top" alt="${producto.nombre} ${producto.talle}"></li>
             <li>Talle: ${producto.talle}</li>
             <li>Precio: $${producto.importe} </li>
+            <li>Cantidad: ${producto.cantidad} </li>
+            <li> <a class="btn btn-primary" id="btn-eliminar${producto.id}">Borrar del carrito</a></li>
         </ul>`;
     })
+    localStorage.setItem("carrito",JSON.stringify(carrito))
+    borrarCarrito()
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function borrarCarrito() {
+    carrito.forEach((producto) => {
+        document.querySelector(`#btn-eliminar${producto.id}`).addEventListener("click", () => {
+            carrito = carrito.filter((productoFilter)=>productoFilter.id !== producto.id )
+            visualizarCarrito()
+        })
+    })
+}
 
 function agregarProducto() {
     let descripcion = prompt(" ingrese nombre de la prenda de ropa")
@@ -101,7 +107,6 @@ function cargarProducto() {
     }
 }
 
-listaHtml()
 function buscarProducto() {
     let buscar = prompt("ingresa el nombre del producto a buscar");
     for (let producto of productos) {
@@ -124,17 +129,16 @@ function filtrarProductosPrecio() {
     const lista = document.getElementById("lista")
     lista.innerHTML = ""
     resultado.forEach(producto => {
-        lista.innerHTML += `   <div class="card" style="width: 16rem;">
-        <img src="..." class="card-img-top" alt="${producto.nombre} ${producto.talle}">
+        lista.innerHTML +=  `   <div class="card" style="width: 16rem;">
+        <img src="assets/productos/prod${producto.id}.jpg" class="card-img-top" alt="${producto.nombre} ${producto.talle}">
         <div class="card-body ">
-            <h5 class="card-title">${producto.nombre}</h5>
-            <p class="card-text">${producto.importe}</p>
-            <a class="btn btn-primary">Comprar</a>
+        <h5 class="card-title">${producto.nombre}</h5>
+        <p class="card-text">$ ${producto.importe}</p>
+        <a class="btn btn-primary" id="btn-agregar${producto.id}">Agregar al carrito</a>
         </div>
-    </div>`
+        </div>`
     })
 }
-
 function ordenarProductosMin() {
     console.table(productos)
     let orden = true
@@ -152,10 +156,6 @@ function ordenarProductosMin() {
     }
     listaHtml()
 }
-const carritoDiv = document.querySelector("#container")
-const boton = document.querySelector("#botn")
-boton.addEventListener("click", cargarProducto)
-const precio_menor = document.querySelector("#menorPrecio")
-precio_menor.addEventListener("click", ordenarProductosMin)
-const precio_mayor = document.querySelector("#mayorPrecio")
-precio_mayor.addEventListener("click", filtrarProductosPrecio)
+//LLamado funciones
+visualizarCarrito()
+listaHtml()
